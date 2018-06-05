31698507820592792130e3abf172ee0d125a6b15
Dmitry Khotyanovich <dmitry@stridertech.co.za>
2018-06-05 22:37:58 +0300

    initial commit

---
+++ TextScanner/TextScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextScanner
+//
+//  Created by Dmitry Khotyanovich on 6/5/18.
+//  Copyright © 2018 OCSICO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
