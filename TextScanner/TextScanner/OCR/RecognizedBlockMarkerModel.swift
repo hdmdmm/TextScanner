2f808cbe1b9684091d15f71e44d88b004f767933
Dmitry Khotyanovich <dmitry@stridertech.co.za>
2018-06-06 00:27:23 +0300

    added markers as view+model not MVVM

---
+++ TextScanner/TextScanner/OCR/RecognizedBlockMarkerModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecognizedBlockModel.swift
+//  Receipts
+//
+//  Created by Dmitry Khotyanovich on 6/5/18.
+//  Copyright © 2018 OCSICO. All rights reserved.
+//
+
+import UIKit
+
+struct RecognizedBlockMarkerModel {
+    let bounds: CGRect
+    let value: String
+}
---
+++ TextScanner/TextScanner/OCR/RecognizedBlockMarkerView.swift
@@ -0,0 +1,30 @@
+//
+//  RecognizedBlockView.swift
+//  Receipts
+//
+//  Created by Dmitry Khotyanovich on 6/5/18.
+//  Copyright © 2018 OCSICO. All rights reserved.
+//
+
+import UIKit
+
+protocol RecognizedMarkerHandlerEvent {
+    func tapEventHandled(view: RecognizedBlockMarkerView, model: RecognizedBlockMarkerModel?)
+}
+
+class RecognizedBlockMarkerView: UIView {
+    var delegate: RecognizedMarkerHandlerEvent? = nil
+    var model: RecognizedBlockMarkerModel? = nil
+    
+    convenience init(frame: CGRect, coefficient: CGFloat) {
+        let frame = frame.multiply(coefficient)
+        self.init(frame: frame)
+        self.backgroundColor = UIColor.green.withAlphaComponent(0.3)
+        
+        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped(recognizer:))))
+    }
+    
+    @objc func tapped(recognizer: UITapGestureRecognizer) {
+        delegate?.tapEventHandled(view: self, model: model)
+    }
+}
