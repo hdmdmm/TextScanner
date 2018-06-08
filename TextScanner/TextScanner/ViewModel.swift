563901d0e19ec369a30c26f24b1876788f7aafe6
Dmitry Khotyanovich <dmitry@stridertech.co.za>
2018-06-08 05:57:19 +0300

    Wizard. Added models.

---
+++ TextScanner/TextScanner/ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModel.swift
+//  TextScanner
+//
+//  Created by Dmitry Khotyanovich on 6/8/18.
+//  Copyright © 2018 OCSICO. All rights reserved.
+//
+
+import UIKit
+
+struct ValueModel {
+    let title: String
+    var value: String?
+}
+
+struct ViewModel {
+    var values: [ValueModel]?
+}
