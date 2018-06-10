a4e1665de5e25bfb5639b3d86813a4411d005141
Dmitry Khotyanovich <dmitry@stridertech.co.za>
2018-06-10 20:20:33 +0300

    How to use wizard.

--- TextScanner/TextScanner/FuelEntryViewController.swift
+++ TextScanner/TextScanner/FuelEntryViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class FuelEntryViewController: UIViewController {
+    
+    var wizardViewModel = WizardViewModel(title: "Fuel entry",
+                                          values: [ValueModel(title: "Price total ($)", value: nil),
+                                                   ValueModel(title: "Amount (L)", value: nil),
+                                                   ValueModel(title: "Date", value: nil),
+                                                   ValueModel(title: "Address", value: nil)] )
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //Display Data from wizardViewModel
+        print(wizardViewModel.values ?? "not initialized wizardViewModel")
+    }
 
     @IBAction func addReceiptActivated(_ sender: Any) {
         let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "WizardViewController")
+        (vc as? WizardViewController)?.inject(model: wizardViewModel)
         present(UINavigationController(rootViewController: vc), animated: false, completion: nil)
     }
-    
 }
